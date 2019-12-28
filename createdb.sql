e8d683ed9cb58286929b6f13a6b82f3d189878de
marc <marc@rdbms.telus.local>
2019-12-27 17:31:29 -0800

    MariaDB [FileHash]> select fqpn,size,mtime,hash from vw_fqpn; +----------------------+------+------------+-------------------+ | fqpn                 | size | mtime      | hash              | +----------------------+------+------------+-------------------+ | budweiser:/etc/fstab |   32 | 1598764320 | 01234556789ABCDEF | +----------------------+------+------------+-------------------+ 1 row in set (0.001 sec)

--- createdb.sql
+++ createdb.sql
@@ -9,15 +9,7 @@ CREATE TABLE `hosts`
 ,`id` BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT
 ) ENGINE=MyISAM;
 
-
-INSERT INTO `hosts` (`hn`)
-VALUES ('budweiser')
-,('labatts')
-,('canadian')
-,('losmuertos')
-;
-
--- SELECT * from `hosts`;
+SELECT * from `hosts`;
 
 --------------------------------------------------------------------------------
 -- dirs
@@ -28,90 +20,85 @@ CREATE TABLE `dirs`
 ) ENGINE=MyISAM;
 
 
-INSERT INTO `dirs` (`dn`)
-VALUES ('/')
-,('/bin')
-,('/etc')
-,('/usr')
-;
-
--- SELECT * from `dirs`;
-
 --------------------------------------------------------------------------------
 -- dir_on_host
 --------------------------------------------------------------------------------
 CREATE TABLE `dir_on_host`
-(`hostid` BIGINT NOT NULL
-,`dirid` BIGINT NOT NULL
+(`hid` BIGINT NOT NULL
+,`did` BIGINT NOT NULL
 ,`id` BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT
-,CONSTRAINT `pk_hostid`
- FOREIGN KEY (`hostid`)
+,CONSTRAINT `pk_hid`
+ FOREIGN KEY (`hid`)
  REFERENCES `hosts` (`id`)
  ON DELETE CASCADE
-,CONSTRAINT `pk_dirid`
- FOREIGN KEY (`dirid`)
+,CONSTRAINT `pk_did`
+ FOREIGN KEY (`did`)
  REFERENCES `dirs` (`id`)
 ) ENGINE=MyISAM;
 
-INSERT INTO `dir_on_host` (`hostid`,`dirid`)
-VALUES (1,1)
-,(1,2)
-,(1,3)
-,(2,1)
-,(2,4)
-,(3,1)
-,(3,3)
-,(3,4)
-;
-
-CREATE VIEW `fqdn` AS
-SELECT CONCAT(`hn`,':',`dn`) AS `fqdn`
+CREATE VIEW `vw_fqdn` (`id`,`hn`,`dn`,`fqdn`,`hid`,`did`) AS
+SELECT `dh`.`id`
+,`h`.`hn`
+,`d`.`dn`
+,CONCAT(`h`.`hn`,':',`d`.`dn`)
+,`dh`.`hid`
+,`dh`.`did`
 FROM `dir_on_host` `dh`
 INNER JOIN `hosts` `h`
-ON `dh`.`hostid`=`h`.`id`
+ON `dh`.`hid`=`h`.`id`
 INNER JOIN `dirs` `d`
-ON `dh`.`dirid`=`d`.`id`;
-
--- SELECT * FROM `dir_on_host`;
-
---------------------------------------------------------------------------------
--- fstats
---------------------------------------------------------------------------------
-CREATE TABLE `fstats`
-(`size` BIGINT NOT NULL
-,`mtime` BIGINT NOT NULL
-,`id` BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT
-) ENGINE=MyISAM;
-
-
---------------------------------------------------------------------------------
--- hashes
---------------------------------------------------------------------------------
-CREATE TABLE `hashes`
-(`hash` VARCHAR(64) UNIQUE NOT NULL
-,`id` BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT
-) ENGINE=MyISAM;
-
+ON `dh`.`did`=`d`.`id`
+;
 
 --------------------------------------------------------------------------------
 -- file_in_dir
 --------------------------------------------------------------------------------
 CREATE TABLE `file_in_dir`
-(`dirhostid` BIGINT UNSIGNED NOT NULL
-,`hashid` BIGINT UNSIGNED NOT NULL
-,`filename` TEXT NOT NULL
+(`dhid` BIGINT UNSIGNED NOT NULL
+,`hash` VARCHAR(64) NOT NULL
+,`fn` TEXT NOT NULL
 ,`size` BIGINT UNSIGNED NOT NULL
 ,`mtime` INT UNSIGNED NOT NULL
 ,`id` BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT
-,CONSTRAINT `pk_dirhostid`
- FOREIGN KEY (`dirhostid`)
+,CONSTRAINT `pk_dhid`
+ FOREIGN KEY (`dhid`)
  REFERENCES `dir_on_host` (`id`)
  ON DELETE CASCADE
 ) ENGINE=MyISAM;
 
--- CREATE VIEW `fqpn` AS
--- (SELECT (CONCAT(`fqdn`,'/',`filename`)) AS `fqpn`
--- FROM file_in_dir `fd`
--- INNER JOIN `fqdn` `fqdn`
--- ON `fd`.`dirhostid`=`fqdn`.`id`
--- );
+CREATE VIEW `vw_fqpn` (`id`,`hn`,`dn`,`fn`,`size`,`mtime`,`hash`,`fqdn`,`fqpn`) AS
+SELECT `fd`.`id`
+,`vw_fqdn`.`hn`
+,`vw_fqdn`.`dn`
+,`fd`.`fn`
+,`fd`.`size`
+,`fd`.`mtime`
+,`fd`.`hash`
+,`vw_fqdn`.`fqdn`
+,(CONCAT(`vw_fqdn`.`fqdn`,'/',`fd`.`fn`))
+FROM file_in_dir `fd`
+INNER JOIN `vw_fqdn`
+ON `fd`.`dhid`=`vw_fqdn`.`id`
+;
+
+--DBG--  --------------------------------------------------------------------------------
+--DBG--  -- TESTS
+--DBG--  -- 1a
+--DBG--  INSERT INTO `hosts` (`hn`)
+--DBG--  VALUES ('budweiser'),('labatts'),('canadian'),('losmuertos');
+--DBG--  -- 1b
+--DBG--  INSERT INTO `dirs` (`dn`)
+--DBG--  VALUES ('/'),('/etc'),('/bin'),('/foo/bar');
+--DBG--  -- 1c
+--DBG--  insert into dir_on_host (hid,did)
+--DBG--  VALUES ((SELECT hosts.id FROM hosts WHERE hosts.hn = 'budweiser'),(SELECT dirs.id FROM dirs WHERE dirs.dn = '/etc'));
+--DBG--  -- 1.
+--DBG--  SELECT * FROM vw_fqdn;
+--DBG--  -- 2a
+--DBG--  INSERT INTO `file_in_dir` (`dhid`,`hash`,`fn`,`size`,`mtime`)
+--DBG--  VALUES ((SELECT id FROM vw_fqdn WHERE fqdn = 'budweiser:/etc')
+--DBG--  ,'01234556789ABCDEF','fstab',32,1598764320)
+--DBG--  ;
+--DBG--  -- 2.
+--DBG--  SELECT * FROM vw_fqpn;
+--DBG--  --------------------------------------------------------------------------------
